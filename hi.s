4a4943950691c425174e4ff3377359d7a5a95f24
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-12-01 13:55:52 +0000

    yoyoyo

--- hi.s
+++ hi.s
@@ -8,10 +8,29 @@ LCD_BASE:    DEFW 0xFF40
 TEMP1:       DEFW 0
 
 START:
-    ; Print the name to the LCD (uses R2 as the LCD pointer register)
-; Print string: "arpan"
+; Clear LCD
 __PTR_LCD_BASE_1:    DEFW LCD_BASE
     LD R2, __PTR_LCD_BASE_1
+    MOV R1, #8
+    ADD R1, R1, R1
+    ADD R1, R1, R1
+    MOV R3, #15
+CLEAR_LOOP1:
+    ST R1, [R2]
+    ADD R2, R2, #1
+    SUB R3, R3, #1
+    BNE CLEAR_LOOP1
+    MOV R3, #5
+CLEAR_LOOP2:
+    ST R1, [R2]
+    ADD R2, R2, #1
+    SUB R3, R3, #1
+    BNE CLEAR_LOOP2
+
+    ; Print the name to the LCD (uses R2 as the LCD pointer register)
+; Print string: "arpan"
+__PTR_LCD_BASE_2:    DEFW LCD_BASE
+    LD R2, __PTR_LCD_BASE_2
     MOV R1, #15
     ADD R1, R1, #15
     ADD R1, R1, #15
@@ -63,8 +82,8 @@ __PTR_LCD_BASE_1:    DEFW LCD_BASE
     ; --- SAVE/RESTORE test ---
     ; Put a known value in R1, save it to TEMP1, clobber R1, then restore
     MOV R1, #7
-__PTR_TEMP1_2:    DEFW TEMP1
-    LD R3, __PTR_TEMP1_2
+__PTR_TEMP1_3:    DEFW TEMP1
+    LD R3, __PTR_TEMP1_3
     ST R1, [R3]
     ; put 'A' (65) in R1 using the macro
     MOV R1, #15
@@ -73,16 +92,16 @@ __PTR_TEMP1_2:    DEFW TEMP1
     ADD R1, R1, #15
     ADD R1, R1, #5
 ; Print string: "A"
-__PTR_LCD_BASE_3:    DEFW LCD_BASE
-    LD R2, __PTR_LCD_BASE_3
+__PTR_LCD_BASE_4:    DEFW LCD_BASE
+    LD R2, __PTR_LCD_BASE_4
     MOV R1, #15
     ADD R1, R1, #15
     ADD R1, R1, #15
     ADD R1, R1, #15
     ADD R1, R1, #5
     ST R1, [R2]
-__PTR_TEMP1_4:    DEFW TEMP1
-    LD R3, __PTR_TEMP1_4
+__PTR_TEMP1_5:    DEFW TEMP1
+    LD R3, __PTR_TEMP1_5
     LD R1, [R3]
 
     ; Stop here
